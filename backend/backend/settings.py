7c24d657416073e8d15c9f6866b884d74692e4fe
cladur <cl4dur@gmail.com>
2022-10-29 22:47:45 +0200

    fix(users): User inherits from AbstractUser now

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://localhost:3000'
 ]
 
+AUTH_USER_MODEL = 'opcol.User'
--- backend/opcol/models.py
+++ backend/opcol/models.py
@@ -1,6 +1,6 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from backend import settings
 from datetime import date
 
@@ -30,15 +30,11 @@ class Opinion(models.Model):
     def __str__(self):
         return self.product.name
 
-# We could use the built-in User model, but it does too much work for us.
-# So instead we create our own model that extends from the AbstractBaseUser.
 
-
-class User(AbstractBaseUser):
+class User(AbstractUser):
     is_admin = models.BooleanField(default=False)
-    username = models.CharField(max_length=60)
     date_joined = models.DateField(("data"), auto_now=False, auto_now_add=True)
-    # W przyszlosci bedzie szyfrowane
+    # Password will be hashed in the future
     password_hash = models.CharField(max_length=60)
 
     def __str__(self):
