90663a46849d6c9e382f531c7fb579c6a08de57f
cladur <cl4dur@gmail.com>
2022-10-30 15:27:22 +0100

    feat(frontend): added dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import { ToastContainer } from "react-toastify";
 
 import Root from "./Root";
 
-import Navigation from "./components/Navigation";
+import requireAuth from "./utils/RequireAuth";
+
 import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
@@ -13,20 +14,16 @@ import Dashboard from "./components/dashboard/Dashboard";
 import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8000";
 
-// axios.defaults.xsrfCookieName = "csrftoken";
-// axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Navigation />
         <Root>
           <ToastContainer hideProgressBar={true} newestOnTop={true} />
           <Switch>
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
-            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/dashboard" component={requireAuth(Dashboard)} />
             <Route exact path="/" component={Home} />
             <Route path="*">
               <h1 className="text-center">
