cf8657aaf94041230182f59897c8af6408c9ae25
Merloran <poncyleuszmaciej@gmail.com>
2022-11-08 01:30:41 +0100

    Update ProductView
    Updated Product View with add opinion modal window

---
+++ frontend/src/components/opinion/tags.js
@@ -0,0 +1,29 @@
+export const positives = [
+  {
+    id: 1,
+    name: "Cheap",
+  },
+  {
+    id: 2,
+    name: "Pretty",
+  },
+  {
+    id: 3,
+    name: "High Quality",
+  }
+];
+
+export const negatives = [
+  {
+    id: 1,
+    name: "Expensive",
+  },
+  {
+    id: 2,
+    name: "Fragile",
+  },
+  {
+    id: 3,
+    name: "Ugly",
+  },
+];
--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -3,8 +3,16 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { getProduct } from "../product_api/ProductsActions";
+import OpinionWindow from "../opinion/Opinion";
+import Button from "react-bootstrap/Button";
+import "./Product.css"
+import testImage from "./testProductImage.jpeg"
 
 class Product extends Component {
+  isAuthenticated() {
+    return localStorage.getItem("token") !== null;
+  }
+  
   componentDidMount() {
     this.props.getProduct(this.props.match.params.id);
   }
@@ -17,15 +25,75 @@ class Product extends Component {
     }
 
     const product = products[0];
-
-    return (
-      <div>
-        <h2>Product</h2>
-        <p>ID: {product.id}</p>
-        <p>Name: {product.name}</p>
-        <p>Description: {product.description}</p>
-      </div>
-    );
+	if (this.isAuthenticated())
+	{
+		return (
+		<div className="mainDiv">
+			<h1>{product.name}</h1>
+			<div className="lrContainer">
+				<div className="leftDiv">
+					<testImage/>
+					<img src={testImage} width="200px" height="200px" alt="tu zdjecie"></img>
+				</div>
+				<div className="rightDiv">
+					<h2>Description</h2>
+					<p>{product.description}</p>
+					<h2>Ingrediens</h2>
+					<p>Jakis kwas i mąka</p>
+					<Button variant="primary">Suggestion?</Button>
+				</div>
+			</div>
+			<h2>Opinions <OpinionWindow /></h2> 
+			<div className="opinion">
+				<p>TestUserName</p>
+				<p>Test opinion on this product</p>
+				<p>Plusy</p>
+				<ul>
+					<li>smaczne</li>
+					<li>biale</li>
+				</ul>
+				<p>Minusy</p>
+				<ul>
+					<li>wypala oczy</li>
+					<li>smierdzi jak skarpetki sprintera</li>
+				</ul>
+			</div>
+		</div>
+		);
+	}
+	return (
+	<div className="mainDiv">
+		<h1>{product.name}</h1>
+		<div className="lrContainer">
+			<div className="leftDiv">
+				<testImage/>
+				<img src={testImage} width="200px" height="200px" alt="tu zdjecie"></img>
+			</div>
+			<div className="rightDiv">
+				<h2>Description</h2>
+				<p>{product.description}</p>
+				<h2>Ingrediens</h2>
+				<p>Jakis kwas i mąka</p>
+				<Button variant="primary">Suggestion?</Button>
+			</div>
+		</div>
+		<h2>Opinions</h2> 
+		<div className="opinion">
+			<p>TestUserName</p>
+			<p>Test opinion on this product</p>
+			<p>Plusy</p>
+			<ul>
+				<li>smaczne</li>
+				<li>biale</li>
+			</ul>
+			<p>Minusy</p>
+			<ul>
+				<li>wypala oczy</li>
+				<li>smierdzi jak skarpetki sprintera</li>
+			</ul>
+		</div>
+	</div>
+	);
   }
 }
 
