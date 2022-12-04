d45d19fb118ba2eaaa7ffe09e4cbcd906b8a5ec6
Anzako <antoluczak01@gmail.com>
2022-12-04 23:49:02 +0100

    feat(frontend) Adding Products button

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -4,12 +4,43 @@ import { Button, Card, Container, Form, InputGroup } from "react-bootstrap";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import defaultImage from "../../assets/img/default.svg";
-import { getProducts } from "../product_api/ProductsActions";
+import { getProducts, addProduct } from "../product_api/ProductsActions";
+import Modal from "react-bootstrap/Modal";
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+      name: "",
+      description: "",
+    };
+  }
+
   componentDidMount() {
     this.props.getProducts();
   }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  onAddClick = () => {
+    const product = {
+      name: this.state.name,
+      description: this.state.description,
+    };
+    this.props.addProduct(product);
+    this.hideModal();
+  };
 
   render() {
     const { products } = this.props.products;
@@ -25,12 +56,19 @@ class Search extends Component {
           <Button variant="outline-secondary" id="button-addon2">
             Search
           </Button>
+          <Button
+            variant="outline-secondary"
+            id="button-addon2"
+            onClick={this.showModal}
+          >
+            Add Product
+          </Button>
         </InputGroup>
         <div style={{ display: "flex", flexWrap: "wrap" }}>
           {products.map((product) => (
             <Card style={{ width: "18rem", margin: "1rem" }} key={product.id}>
               <Card.Link href={"/products/" + product.id}>
-                <Card.Img variant="top" src={defaultImage}/>
+                <Card.Img variant="top" src={defaultImage} />
               </Card.Link>
               <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
@@ -40,6 +78,43 @@ class Search extends Component {
             </Card>
           ))}
         </div>
+        <Modal show={this.state.show} onHide={this.hideModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add product</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="contentId">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={1}
+                  name="name"
+                  placeholder="Enter name"
+                  value={this.name}
+                  onChange={this.onChange}
+                />
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="description"
+                  placeholder="Enter description"
+                  value={this.description}
+                  onChange={this.onChange}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.hideModal}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.onAddClick}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     );
   }
@@ -47,6 +122,7 @@ class Search extends Component {
 
 Search.propTypes = {
   getProducts: PropTypes.func.isRequired,
+  addProduct: PropTypes.func.isRequired,
   products: PropTypes.object.isRequired,
 };
 
@@ -56,4 +132,5 @@ const mapStateToProps = (state) => ({
 
 export default connect(mapStateToProps, {
   getProducts,
+  addProduct,
 })(withRouter(Search));
